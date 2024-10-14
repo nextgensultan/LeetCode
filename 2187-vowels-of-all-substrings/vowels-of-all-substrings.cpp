c3e57b3cc09a032af91de01d9f42b78e82fb9aed
Ibrahim Sultan <74836272+nextgensultan@users.noreply.github.com>
2024-10-14 13:48:11 +0500

    Time: 28 ms (22.33%) | Memory: 12.5 MB (53.02%) - LeetSync

--- 2187-vowels-of-all-substrings/vowels-of-all-substrings.cpp
+++ 2187-vowels-of-all-substrings/vowels-of-all-substrings.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     long long countVowels(string word) {
         int n = word.size();
-        long long result =0;
-        for(int i=0;i<n;i++)
-        {
-            if(word[i] == 'a' || word[i]=='e' || word[i] == 'i' || word[i]=='o' || word[i] == 'u' )
-            result +=  (long long)1 * (i+1) * (n-i);
+        long long result = 0;
+        for (int i = 0; i < n; i++) {
+            if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' ||
+                word[i] == 'o' || word[i] == 'u')
+                result += (long long)1 * (i + 1) * (n - i);
         }
         return result;
     }
